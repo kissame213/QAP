6dd436039fca7c2d0743c4efc3ee7e5a0d451c9c
kissame213 <lord3418@mail.ru>
2023-03-09 22:11:31 +0500

    Create config.py

---
+++ currency_bot/config.py
@@ -0,0 +1,13 @@
+APIKEY = 'uAv3gnPBjadukNpfyTxkfirCqIWOskuu'
+TOKEN = '6288112020:AAElRFoMUySSOx9ghXdJM3LdwUc8hj84NUo'
+keys = {
+    "юань": "CNY",
+    "долларСША": "USD",
+    "евро": "EUR",
+    "йена": "JPY",
+    "рубль": "RUB",
+    "вона": "KRW",
+    "злотый": "PLN",
+    "форинт": "HUF",
+    "дирхам": "AED",
+}
