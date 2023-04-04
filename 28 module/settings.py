65daaf70dc0b209da1a76223a58e76afdda1e232
kissame213 <lord3418@mail.ru>
2023-04-04 21:41:38 +0500

    Create settings.py

---
+++ 28 module/settings.py
@@ -0,0 +1,26 @@
+class Settings:
+    chrome_driver = "/tests/chromedriver.exe"
+
+    base_url = 'https://b2c.passport.rt.ru'
+    valid_email = 'lo****@gmail.com'
+    valid_password = 'BiTj2vWpJ.cK2.N'
+    valid_email_reg = 'Ввести валидный email'
+    invalid_email = 'incorrect@mmail.ru'
+    invalid_password = 'dfawe121'
+    empty_email = ''
+    empty_password = ''
+    menu_type_auth = ['Телефон', 'Почта', 'Логин', 'Лицевой счёт']
+    placeholder_name = ['Мобильный телефон', 'Электронная почта', 'Логин', 'Лицевой счёт']
+    first_name = 'Павел'
+    last_name = 'Савельев'
+    first_name_ = '-'
+    last_name_ = '-'
+    russian_generate_string = 'л'
+    latin_generate_string = 'incorrect'
+    chinese_chars = '形聲字象形字形声字/'
+    special_chars = '()_+=/{}!@#$%^&*'
+    numbers = 845
+    empty = ''
+    passw1 = 'BiTj2vWpJ.cK2.N'
+    passw2 = 'Cqwer4ds4'
+    passw3 = 'yy65vx1212'
